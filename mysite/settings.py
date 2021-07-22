cfe6bb3bbe66d263e79bf0097c9861534bc3d5f9
Your Name <you@example.com>
2021-07-22 13:06:55 +0530

    10.0.133.90

--- mysite/settings.py
+++ mysite/settings.py
@@ -97,10 +97,21 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'sampledb',
+        'USER': 'justuser',
+        'PASSWORD': 'qwerty',
+        'HOST': '10.0.133.90',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
